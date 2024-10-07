af71e0f736ad44985c2cab55fc055f80b71de6d4
NoufBinHassan <noufapplebh@gmail.com>
2024-10-07 12:28:32 +0300

    add page 5 and 6

---
+++ repo2/page5.swift
@@ -0,0 +1,18 @@
+//
+//  page5.swift
+//  repo2
+//
+//  Created by Nouf Bin Hassan on 07/10/2024.
+//
+
+import SwiftUI
+
+struct page5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    page5()
+}
---
+++ repo2/page6.swift
@@ -0,0 +1,18 @@
+//
+//  page6.swift
+//  repo2
+//
+//  Created by Nouf Bin Hassan on 07/10/2024.
+//
+
+import SwiftUI
+
+struct page6: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    page6()
+}
