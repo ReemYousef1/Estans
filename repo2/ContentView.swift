649eed2fa1b30006c4f952a6858aaf2d8ee05108
ReemYousef1 <109497565+ReemYousef1@users.noreply.github.com>
2024-10-07 10:49:06 +0300

    Initial Commit

---
+++ repo2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  repo2
+//
+//  Created by Reem on 04/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ repo2/repo2App.swift
@@ -0,0 +1,17 @@
+//
+//  repo2App.swift
+//  repo2
+//
+//  Created by Reem on 04/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct repo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
