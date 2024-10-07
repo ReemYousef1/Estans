2bfe31382b2f1674cbd543eec09a4d16e6aa675e
ANOUD <a20nood@gmail.com>
2024-10-07 11:43:44 +0300

    Add Page1

---
+++ repo2/Page1View.swift
@@ -0,0 +1,18 @@
+//
+//  Page1View.swift
+//  repo2
+//
+//  Created by Alanoud Alamrani on 04/04/1446 AH.
+//
+
+import SwiftUI
+
+struct Page1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Page1View()
+}
