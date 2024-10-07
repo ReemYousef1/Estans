de49532328220bb1c01f9e79ec9836718d20d480
ANOUD <a20nood@gmail.com>
2024-10-07 12:44:50 +0300

    Page3

---
+++ repo2/page3.swift
@@ -0,0 +1,18 @@
+//
+//  page3.swift
+//  repo2
+//
+//  Created by Alanoud Alamrani on 04/04/1446 AH.
+//
+
+import SwiftUI
+
+struct page3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    page3()
+}
---
+++ repo2/page7.swift
@@ -0,0 +1,18 @@
+//
+//  page7.swift
+//  repo2
+//
+//  Created by Alanoud Alamrani on 04/04/1446 AH.
+//
+
+import SwiftUI
+
+struct page7: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    page7()
+}
